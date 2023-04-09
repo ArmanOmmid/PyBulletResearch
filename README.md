d107169bc306e91f340034299cc8e77c7994961f
Arman Ommid <armanommid@gmail.com>
2023-04-08 23:35:58 -0700

    start

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # PyBulletResearch
+
+# Goal:
+
+The robotics project would start like this: in Pybullet, build a
+simulated ground vehicle that is supposed to go
+from an initial location to some goal location, and there are random
+static obstacles in between. Then have this environment connected with
+OpenAI Gym so that you can run standard policy gradient RL algorithms
+such as PPO and SAC to train the controller for the car so that it
+goes from the initial state to the goal and avoid the obstacles.
+
+Getting familiar with these tools may require quite some effort, so
+take your time and let me know when you have something working.
+
+(something like this
+https://urldefense.com/v3/__https://www.youtube.com/watch?v=tozzb6xC1LI__;!!Mih3wA!Buejbwp2bgHIMcIvfX7xWLQr6D-qXoKnlnkY_rIRrQEL0KTlHL8E_qtZHCSQTc3S3kCAQtqNhRyLrw$ )
---
+++ test2.py
@@ -0,0 +1,28 @@
+
+import numpy as np
+import pybullet as p
+import pybullet_data
+import time
+
+# Setpu simulation
+p.connect(p.GUI) # Gives us visualations of our simulation if we want it. Use p.DIRECT if we don't want to render graphics. 
+p.resetSimulation()
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0, 0, -9.8)
+p.setRealTimeSimulation(0) # Not a real time simulation
+
+# load assets 
+p.loadURDF("plane.urdf", [0, 0, 0], [0, 0, 0, 1]) # Instantiate with xyz coordinates and nijk orientation (quaternion). Defaults are [0, 0, 0] and [0, 0, 0, 1]
+targid = p.loadURDF("franka_panda/panda.urdf", [0, 0, 0], [0, 0, 0, 1], useFixedBase = True) # Robotic Arm. Fixed Base means that the base of the object does not move (but is still physical)
+obj_of_focus = targid
+
+p.getNumJoints(targid)
+jointid = 4
+jlower = p.getJointInfo(targid, jointid)[8] # Create joints for Robotic Arm
+jupper = p.getJointInfo(targid, jointid)[9]
+
+# for step in range(300):
+#     focus_position, focus_orientation = p.getBasePositionAndOrientation(targid)
+#     p.resetDebugVisualizerCamera(cameraDistance=3, CameraYaw=0, cameraPitch=-40, cameraTargetPosition=focus_position) # Follow the robotic arm 
+#     p.stepSimulation()
+#     time.sleep(0.1)
