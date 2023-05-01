8a9e85a6f8eb900702640a308fda5f33af3f57ce
Arman Ommid <armanommid@gmail.com>
2023-04-30 19:51:14 -0700

    prtogress

--- Tutorials/Ant/ant.py
+++ Tutorials/Ant/ant.py
@@ -17,8 +17,8 @@ parser.add_argument('type', type=str,
                     help='Train or Test')
 parser.add_argument('epochs', type=int,
                     help='Epochs')
-parser.add_argument('iterations', type=int,
-                    help='Iterations')
+parser.add_argument('--load', action='store_true',
+                    help='Load')
 parser.add_argument('--render', action='store_true',
                     help='Render')
 
@@ -29,7 +29,7 @@ def main(args):
     test = type in ['test', 'both']
 
     epochs = args.epochs
-    iterations = args.iterations
+    load = args.load
     render = args.render
 
     if not os.path.exists("_weights"):
@@ -39,6 +39,9 @@ def main(args):
     policy_kwargs = dict(activation_fn=nn.LeakyReLU, net_arch=[512, 512])
     model = PPO('MlpPolicy', env, learning_rate=0.0001, policy_kwargs=policy_kwargs, verbose=1)
 
+    if load:
+        model.load("_weights/PPO_Ant_Load.zip")
+
     if render:
         env.render(mode="human")
 
@@ -46,7 +49,7 @@ def main(args):
         MAX_AVERAGE_SCORE = 2000
         for i in range(epochs):
             print("Training iteration: {}".format(i))
-            model.learn(total_timesteps=iterations)
+            model.learn(total_timesteps=10000)
             model.save("_weights/PPO_Ant_Save")
             mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=5)
             print("Mean Reward: {}".format(mean_reward))
@@ -59,7 +62,6 @@ def main(args):
         os.rename("_weights/PPO_Ant_Save", "_weights/PPO_Ant_Load")
 
     if test:
-        model.load("_weights/PPO_Ant_Load.zip")
         mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
 
 if __name__ == "__main__":
